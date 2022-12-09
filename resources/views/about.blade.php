e586b0e65a64f07026519179fd5a8dadd693d875
Bosnich Timoth Bonsaleng <bosnichtimothy4@gmail.com>
2022-12-09 16:48:25 +0700

    edit tampilan

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LBW Transaltor | About</title>
-</head>
-<body>
+@extends('layout.main')
+
+
+@section('container')
     <h1>Halaman About</h1>
-</body>
-</html>
+@endsection
+    
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',[
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about',[
+        "title" => "About"
+    ]);
 });
