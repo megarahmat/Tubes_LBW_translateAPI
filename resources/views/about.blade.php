42783095cf5e5f44a49631aba12903d7e6a2bbbe
megarahmat <ega43@rocketmail.com>
2023-01-04 13:34:37 +0700

    halaman about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,10 @@
 
 @section('container')
     <h1>Halaman About</h1>
-
+    <p>Tugas besar ini dikerjakan oleh :</p>
+    <h5>Mega Rahmat 2017730021</h5>
+    <h5>Bosnich Timothy 2017730086</h5>
+    <h5>Ganda Tua Sinaga 2017730049</h5>
     
 
 @endsection
